baa3ee63706cd3ce3cf4d0fc462a809b796f9839
Diego Berrocal <cestdiego@gmail.com>
2015-11-03 14:23:41 -0500

    Ordering works if you put information manually

--- sweetgreen.el
+++ sweetgreen.el
@@ -1,3 +1,4 @@
+;; -*- lexical-binding: t -*-
 (require 'request)
 (require 'rx)
 
@@ -11,7 +12,7 @@
 (defvar order-id nil)
 (defvar muh-cookie "")
 (defvar items '())
-(defvar le-orders '())
+(defvar orders '())
 (defvar order-id nil)
 (defvar basket-id nil)
 
@@ -79,16 +80,71 @@
             (lambda (&key data response &allow-other-keys)
               (let* ((order     (aref (=> data 'orders) 0))
                      (basket_id (=> order 'basket_id)))
-                (setq basket-id (number-to-string basket_id))
-                (print (basket_id . order))
-                (push `(,basket_id . ,order) le-orders)))))
+                (setq basket-id basket_id)
+                (push `(,basket_id . ,order) orders))))
+ )
 
 (setq order-id nil)
 (setq basket-id nil)
 
-(=> (aref (assoc-default "something" orders) 0) 'basket_id)
-(number-to-string (=> orders 642731 'ignored_order_id))
+(let ((created_at (=> orders basket-id 'created_at))
+      (wanted_time (=> orders basket-id 'earliest_wanted_time))
+      ))
 
-(setq holi '(("hola" . "tuma")
-             ("heyo" . "lelel")))
-(add-to-list 'holi `(,(car (assoc "hola" holi)) . "asdfasd"))
+(--map (cancel-item it) a)
+
+(defun cancel-item (id)
+  (request
+   (concat "https://order.sweetgreen.com/api/line_items/" (number-to-string id))
+   :type "DELETE"
+   :headers `(("Cookie" . ,(concat "_session_id=" muh-cookie))
+              ("X-CSRF-Token" . ,csrf-token))
+
+   :parser 'json-read
+   :status-code '((204 . (lambda (&rest _) (message "Deleted item successfully")))
+                  (500 . (lambda (&rest _) (message "Item doesn't seem to exist"))))))
+
+
+(defun checkout (order)
+  (setq order (=> orders basket-id))
+  (request
+   (concat "https://order.sweetgreen.com/api/line_items/" (=> order 'id))
+   :type "PUT"
+   :headers `(("Cookie" . ,(concat "_session_id=" muh-cookie))
+              ("X-CSRF-Token" . ,csrf-token))
+   :data `(json-encode
+           `(("order" . (("available_wanted_times_tuples" . (=> order 'available_wanted_times_tuples))
+                         ("basket_id" . (=> order 'basket_id))
+                         ("created_at" . (=> order 'created_at))
+                         ("coupon_code" . (=> order 'coupon_code))
+                         ("coupon_discount" . (=> order 'coupon_discount))
+                         ("placed_time" . (=> order 'placed_time))
+                         ("formatted_wanted_time" . (=> order 'formatted_wanted_time))
+                         ("restaurant_id" . (=> order 'restaurant_id))
+                         ("sales_tax" . (=> order 'sales_tax))
+                         ("subtotal" . (=> order 'subtotal))
+                         ("total" . (=> order 'total))
+                         ("shows_feedback_form" . (=> order 'shows_feedback_form))
+                         ("uploaded_at")
+                         ("contact_number" . "***REMOVED***")
+                         ("state" . "complete")
+                         ("wanted_time" . (=> order 'earliest_wanted_time))
+                         ("billing_account" .
+                          (("card_type" . "cash")
+                           ("card_number")
+                           ("zip")
+                           ("last_four")
+                           ("cvv")
+                           ("expiry_month")
+                           ("expiry_year")
+                           ("description" . "sweetgreen Rewards (Pay with App)")
+                           ("save_on_file" . :json-false)
+                           ))))))
+   :parser 'json-read
+   :complete (function*
+              (lambda (&key data response &allow-other-keys)
+                (let* ((order     (aref (=> data 'orders) 0))
+                       (basket_id (=> order 'basket_id)))
+                  (print data)
+                  )))
+  ))
